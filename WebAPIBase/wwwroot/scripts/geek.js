008f6b580184ce3c9e5530bc97f3f32c82a030f8
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2023-11-29 16:34:53 +0100

    geek feltöltés

---
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -0,0 +1,31 @@
+﻿
+window.onload = function () {
+     fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+}
+
+var viccek;
+
+
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    const viccekContainer = document.getElementById('viccekContainer');
+
+    for (const vicc of d) {
+        // Létrehozunk egy div elemet a viccnek
+        const viccElem = document.createElement('div');
+
+        // Beállítjuk a tartalmát a vicc szövegére
+        viccElem.innerText = `${vicc.question} ${vicc.answer}`;
+
+        // Hozzáadjuk az új elemet a konténerhez
+        viccekContainer.appendChild(viccElem);
+    }
+}
+
