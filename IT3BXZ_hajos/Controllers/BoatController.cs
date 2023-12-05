06988b6830eef6fd055bc6dc5f345e911b1d0fbb
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2023-12-05 19:53:08 +0100

    layout-ig

--- IT3BXZ_hajos/Controllers/BoatController.cs
+++ IT3BXZ_hajos/Controllers/BoatController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IT3BXZ_hajos.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IT3BXZ_hajos.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BoatController : ControllerBase
+    [HttpGet]
+    [Route("questions/all")]
+    public IActionResult MindegyHogyHivjak()
     {
+        HajosContext context = new HajosContext();
+        var kérdések = from x in context.Questions select x.Question1;
+
+        return Ok(kérdések);
     }
 }
