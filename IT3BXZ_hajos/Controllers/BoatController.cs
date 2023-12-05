51d4273fe4283301f153fb46f3579094c4ab0a51
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2023-12-05 20:34:44 +0100

    hajos vegleges

--- IT3BXZ_hajos/Controllers/BoatController.cs
+++ IT3BXZ_hajos/Controllers/BoatController.cs
@@ -4,13 +4,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IT3BXZ_hajos.Controllers
 {
-    [HttpGet]
-    [Route("questions/all")]
-    public IActionResult MindegyHogyHivjak()
-    {
-        HajosContext context = new HajosContext();
-        var kérdések = from x in context.Questions select x.Question1;
 
-        return Ok(kérdések);
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/{sorszvám}")]
+        public IActionResult M1()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M2(int sorszám)
+        {
+            HajosContext context = new HajosContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
+
+        [HttpGet]
+        [Route("questions/count")]
+        public int M4() //Tetszőleges metódusnév
+        {
+            HajosContext context = new HajosContext();
+            int kérdésekSzáma = context.Questions.Count();
+
+            return kérdésekSzáma;
+        }
     }
 }
