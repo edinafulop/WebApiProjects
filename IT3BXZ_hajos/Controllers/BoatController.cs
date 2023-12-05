0a2f15f5944a36760e9023e7255c716ce5bab5fa
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2023-12-05 19:38:09 +0100

    Csatlakozás adatbázishoz-ig

---
+++ IT3BXZ_hajos/Controllers/BoatController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IT3BXZ_hajos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+    }
+}
